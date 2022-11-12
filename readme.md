8e96b63dad730b0f825da6c33c94299d590d4c1c
BramMortier <79015250+BramMortier@users.noreply.github.com>
2022-11-12 19:06:05 +0100

    init

---
+++ readme.md
@@ -0,0 +1,46 @@
+# Blitz chat app
+
+# Table of contents
+
+1. [Introduction](#introduction)
+2. [Tech stack](#technologies)
+3. [Running the project](#running-the-project)
+
+<a name="introduction"></a>
+
+## Introduction
+
+This mobile app allows users to create or join chatrooms. Chat with people in real time and customize the look of the app to your linkings
+
+<a name="technologies"></a>
+
+## Technologies used
+
+1. HTML5
+2. Typescript & Sass
+3. Webpack (bundler)
+4. Firebase
+    1. Firestore
+    2. Firebase Auth
+    3. Firebase Hosting
+5. Gsap (animations)
+
+<a name="running-the-project"></a>
+
+## Running the project
+
+Start by installing the node modules
+
+> $ npm i
+
+Want to contribute to the project? start a dev server with:
+
+> $ npm run dev
+
+The dev server doesnt write to the hard drive. To do so run:
+
+> $ npm run build
+
+Want an optimized build?
+
+> $ npm run prod
---
+++ webpack.config.js
@@ -0,0 +1,66 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+    entry: "./src/typescript/index.ts",
+    output: {
+        path: path.resolve(__dirname, "dist"),
+        publicPath: "",
+        filename: "bundle.js",
+    },
+    devServer: {
+        port: 8080,
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        hot: true,
+        compress: true,
+    },
+    devtool: "source-map",
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader,
+                    },
+                    {
+                        loader: "css-loader",
+                    },
+                    {
+                        loader: "postcss-loader",
+                    },
+                    {
+                        loader: "sass-loader",
+                        options: {
+                            implementation: require("sass"),
+                        },
+                    },
+                ],
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)$/,
+                use: [
+                    {
+                        loader: "file-loader",
+                        options: {
+                            outputPath: "images",
+                        },
+                    },
+                ],
+            },
+        ],
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: "bundle.css",
+        }),
+    ],
+    mode: "development",
+};
