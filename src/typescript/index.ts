e794abc5085f7ddb495aa93c1704a3a4be47679f
BramMortier <79015250+BramMortier@users.noreply.github.com>
2022-11-15 17:27:57 +0100

    chatroom page layout started

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -5,3 +5,20 @@ import "../sass/style.scss";
 // module imports
 
 // ------------------------------------------- //
+
+// import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAfLyWlU3BMjia7qU9-qYDGGMuuPEBtFqU",
+//     authDomain: "blitz-chat-d6dc9.firebaseapp.com",
+//     projectId: "blitz-chat-d6dc9",
+//     storageBucket: "blitz-chat-d6dc9.appspot.com",
+//     messagingSenderId: "909983333760",
+//     appId: "1:909983333760:web:be045f1d22c5d9378ed565",
+//     measurementId: "G-8VW8H07175",
+// };
+
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
