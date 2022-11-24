7bc7a97c1a800103ce8ab962ade6351465e4d7a4
BramMortier <79015250+BramMortier@users.noreply.github.com>
2022-11-24 14:16:24 +0100

    module problem

--- src/typescript/index.ts
+++ src/typescript/index.ts
@@ -3,25 +3,11 @@
 import "../sass/style.scss";
 
 // module imports
-// import { registerBtn } from "./lib/constants";
+import { registerBtn, loginBtn, loginPage, chatPage, chatroomsPage, chatrooms, pages } from "./lib/constants";
 // import { Router } from "./lib/navigation";
 // import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 // ------------------------------------------- //
-
-const registerBtn: Element | null = document.getElementById("register-btn");
-const loginBtn: Element | null = document.getElementById("login-btn");
-
-const registerPage: Element | null = document.getElementById("register-page");
-const loginPage: Element | null = document.getElementById("login-page");
-const chatroomsPage: Element | null = document.getElementById("chatrooms-page");
-const chatPage: Element | null = document.getElementById("chat-page");
-const createChatPage: Element | null = document.getElementById("create-chat-page");
-
-const chatroomsList: Element | null = document.getElementById("chatrooms-list");
-const chatrooms: (Element | null)[] = [...document.querySelectorAll(".chatrooms__chat")];
-
-const pages: (Element | null)[] = [registerPage, loginPage, chatroomsPage, chatPage, createChatPage];
 
 class Router {
     constructor() {
@@ -37,19 +23,19 @@ class Router {
     };
 }
 
-class Firebase {
-    // firebaseConfig = {
-    //     apiKey: "AIzaSyAfLyWlU3BMjia7qU9-qYDGGMuuPEBtFqU",
-    //     authDomain: "blitz-chat-d6dc9.firebaseapp.com",
-    //     projectId: "blitz-chat-d6dc9",
-    //     storageBucket: "blitz-chat-d6dc9.appspot.com",
-    //     messagingSenderId: "909983333760",
-    //     appId: "1:909983333760:web:be045f1d22c5d9378ed565",
-    //     measurementId: "G-8VW8H07175",
-    // };
-    // app = initializeApp(firebaseConfig);
-    // analytics = getAnalytics(app);
-}
+// class Firebase {
+//     firebaseConfig = {
+//         apiKey: "AIzaSyAfLyWlU3BMjia7qU9-qYDGGMuuPEBtFqU",
+//         authDomain: "blitz-chat-d6dc9.firebaseapp.com",
+//         projectId: "blitz-chat-d6dc9",
+//         storageBucket: "blitz-chat-d6dc9.appspot.com",
+//         messagingSenderId: "909983333760",
+//         appId: "1:909983333760:web:be045f1d22c5d9378ed565",
+//         measurementId: "G-8VW8H07175",
+//     };
+//     app = initializeApp(this.firebaseConfig);
+//     analytics = getAnalytics(this.app);
+// }
 
 const initApp = (): void => {
     console.log("Site loaded");
